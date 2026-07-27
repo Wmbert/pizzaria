0839ae6bf36c15c08171e4a6aec43755a0282433
Wmbert <wmbert.dev@gmail.com>
2026-07-27 10:39:20 -0300

    create mobile app to pizzaria

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,12 @@
+//_layout.tsx
+
+import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+
+export default function RootLayout(){
+  return(
+    <Stack>
+      <Stack.Screen name="index"/>
+    </Stack>
+  )
+}
